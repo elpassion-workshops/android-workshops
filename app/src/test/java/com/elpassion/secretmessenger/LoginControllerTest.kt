605f56f389b37df251dabe12c33e7bba434625d5
ghostbuster91 <kghost0@gmail.com>
2016-09-02 19:26:39 +0200

    Should call api when onLogin method is called

--- app/src/test/java/com/elpassion/secretmessenger/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/secretmessenger/LoginControllerTest.kt
@@ -1,16 +1,27 @@
 package com.elpassion.secretmessenger
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class LoginControllerTest {
 
     @Test
-    fun initTest() {
-        LoginController()
+    fun shouldCallApiWhenOnLoginMethodIsCalled() {
+        val loginApi = mock<LoginApi>()
+        LoginController(loginApi).onLogin()
+        verify(loginApi, times(1)).login()
     }
 
 }
 
-class LoginController {
+interface LoginApi {
+    fun login()
+}
 
+class LoginController(val loginApi: LoginApi) {
+    fun onLogin() {
+        loginApi.login()
+    }
 }
