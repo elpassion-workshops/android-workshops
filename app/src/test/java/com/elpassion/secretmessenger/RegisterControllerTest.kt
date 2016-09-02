bb4e4244a5bbfdb7551954bf6c6205f556c6da79
Karol Kowalski <ordon1313@gmail.com>
2016-09-02 20:13:04 +0200

    Show error after unsuccessful registration

--- app/src/test/java/com/elpassion/secretmessenger/RegisterControllerTest.kt
+++ app/src/test/java/com/elpassion/secretmessenger/RegisterControllerTest.kt
@@ -2,27 +2,57 @@ package com.elpassion.secretmessenger
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
+import rx.Observable
 
 class RegisterControllerTest {
 
+    val api = mock<RegisterApi>()
     val view = mock<RegisterView>()
-    val controller = RegisterController(view)
+    val controller = RegisterController(api, view)
 
     @Test
     fun shouldOpenHomeScreenAfterSuccessfulRegistration() {
+        stubApiForSuccess()
         controller.register()
         verify(view).openHomeScreen()
     }
+
+    @Test
+    fun shouldShowErrorRegistrationErrorAfterUnsuccessfulRegistration() {
+        stubApiForFailure()
+        controller.register()
+        verify(view).showRegisterError()
+    }
+
+    private fun stubApiForSuccess() {
+        whenever(api.register()).thenReturn(Observable.just(Unit))
+    }
+
+    private fun stubApiForFailure() {
+        whenever(api.register()).thenReturn(Observable.error(RuntimeException()))
+    }
+}
+
+interface RegisterApi {
+    fun register(): Observable<Unit>
 }
 
 interface RegisterView {
     fun openHomeScreen()
+    fun showRegisterError()
 }
 
-class RegisterController(val view: RegisterView) {
+class RegisterController(val api: RegisterApi, val view: RegisterView) {
 
     fun register() {
-        view.openHomeScreen()
+        api.register()
+                .subscribe({
+                    view.openHomeScreen()
+                }, {
+                    view.showRegisterError()
+                })
+
     }
 }
