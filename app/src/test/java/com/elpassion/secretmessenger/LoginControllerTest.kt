1fcc56718bef1ab2b7cd53e1d470f2c92e12edf9
ghostbuster91 <kghost0@gmail.com>
2016-09-02 19:21:27 +0200

    Added init test to force adding kotlin to project

---
+++ app/src/test/java/com/elpassion/secretmessenger/LoginControllerTest.kt
@@ -0,0 +1,16 @@
+package com.elpassion.secretmessenger
+
+import org.junit.Test
+
+class LoginControllerTest {
+
+    @Test
+    fun initTest() {
+        LoginController()
+    }
+
+}
+
+class LoginController {
+
+}
