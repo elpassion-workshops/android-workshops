3a010375df83b6cd31032963851474ec3be80fcc
ghostbuster91 <kghost0@gmail.com>
2016-09-02 19:32:16 +0200

    Should open HomeScreen after login

--- app/src/test/java/com/elpassion/secretmessenger/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/secretmessenger/LoginControllerTest.kt
@@ -10,9 +10,22 @@ class LoginControllerTest {
     @Test
     fun shouldCallApiWhenOnLoginMethodIsCalled() {
         val loginApi = mock<LoginApi>()
-        LoginController(loginApi).onLogin()
+        val view = mock<LoginView>()
+        LoginController(loginApi, view).onLogin()
         verify(loginApi, times(1)).login()
     }
+
+    @Test
+    fun shouldOpenHomeScreenAfterLogin() {
+        val loginApi = mock<LoginApi>()
+        val view = mock<LoginView>()
+        LoginController(loginApi, view).onLogin()
+        verify(view, times(1)).openHomeScreen()
+    }
+}
+
+interface LoginView {
+    fun openHomeScreen()
 
 }
 
@@ -20,8 +33,9 @@ interface LoginApi {
     fun login()
 }
 
-class LoginController(val loginApi: LoginApi) {
+class LoginController(val loginApi: LoginApi, val view: LoginView) {
     fun onLogin() {
         loginApi.login()
+        view.openHomeScreen()
     }
 }
