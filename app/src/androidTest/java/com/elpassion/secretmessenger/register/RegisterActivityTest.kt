00dbd40570a43d1e6a52ddd0a8c407b17721dbba
Karol Kowalski <ordon1313@gmail.com>
2016-09-02 21:56:54 +0200

    Add empty register activity

---
+++ app/src/androidTest/java/com/elpassion/secretmessenger/register/RegisterActivityTest.kt
@@ -0,0 +1,16 @@
+package com.elpassion.secretmessenger.register
+
+import android.support.test.rule.ActivityTestRule
+import com.elpassion.secretmessenger.register.impl.RegisterActivity
+import org.junit.Rule
+import org.junit.Test
+
+class RegisterActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<RegisterActivity>(RegisterActivity::class.java)
+
+    @Test
+    fun startRegisterActivity() {
+    }
+}
---
+++ app/src/main/java/com/elpassion/secretmessenger/register/impl/RegisterActivity.kt
@@ -0,0 +1,7 @@
+package com.elpassion.secretmessenger.register.impl
+
+import android.support.v7.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+}
