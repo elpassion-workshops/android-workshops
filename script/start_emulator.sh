7525802077bd7c948904a5f4960b6ce15b3d0c33
ghostbuster91 <kghost0@gmail.com>
2016-09-06 11:03:42 +0200

    add sleep in travis config before unlocking screen

--- script/start_emulator.sh
+++ script/start_emulator.sh
@@ -2,4 +2,5 @@
 echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a --skin 480x800
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
+sleep 30
 adb shell input keyevent 82
