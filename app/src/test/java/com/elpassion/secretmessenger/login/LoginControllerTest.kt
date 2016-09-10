cac1c9cdf5c59c8e908d1e2f7211cdaecf891057
ghostbuster91 <kghost0@gmail.com>
2016-09-10 14:25:21 +0200

    Call api with correct login

--- app/src/test/java/com/elpassion/secretmessenger/login/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/secretmessenger/login/LoginControllerTest.kt
@@ -1,5 +1,6 @@
 package com.elpassion.secretmessenger.login
 
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
@@ -11,20 +12,28 @@ class LoginControllerTest {
     fun shouldCallApiAfterOnLogin() {
         val api = mock<Login.Api>()
         val controller = LoginController(api)
-        controller.onLogin()
-        verify(api, times(1)).login()
+        controller.onLogin("login")
+        verify(api, times(1)).login(any())
+    }
+
+    @Test
+    fun shouldCallApiWithCorrectLogin() {
+        val api = mock<Login.Api>()
+        val controller = LoginController(api)
+        controller.onLogin(login = "login")
+        verify(api, times(1)).login(login = "login")
     }
 
 }
 
 interface Login {
     interface Api {
-        fun login()
+        fun login(login: String)
     }
 }
 
 class LoginController(val api: Login.Api) {
-    fun onLogin() {
-        api.login()
+    fun onLogin(login: String) {
+        api.login(login)
     }
 }
