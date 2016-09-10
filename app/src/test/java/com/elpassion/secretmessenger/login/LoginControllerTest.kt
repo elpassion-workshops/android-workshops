ebd9ea043c8740b4f885e96f414305c9c49e1809
ghostbuster91 <kghost0@gmail.com>
2016-09-10 14:53:27 +0200

    Open conversation list when api call succeed

--- app/src/test/java/com/elpassion/secretmessenger/login/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/secretmessenger/login/LoginControllerTest.kt
@@ -1,6 +1,8 @@
 package com.elpassion.secretmessenger.login
 
-import com.nhaarman.mockito_kotlin.*
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class LoginControllerTest {
@@ -8,9 +10,18 @@ class LoginControllerTest {
     @Test
     fun shouldCallApiWithCorrectLoginAndPassword() {
         val api = mock<Login.Api>()
-        val controller = LoginController(api)
+        val controller = LoginController(api, mock())
         controller.onLogin(login = "login", password = "password")
         verify(api, times(1)).login(login = "login", password = "password")
+    }
+
+    @Test
+    fun shouldOpenConversationListScreenWhenApiCallSucceed() {
+        val view = mock<Login.View>()
+        val api = mock<Login.Api>()
+        val controller = LoginController(api, view)
+        controller.onLogin("", "")
+        verify(view, times(1)).showConversationList()
     }
 }
 
@@ -18,10 +29,16 @@ interface Login {
     interface Api {
         fun login(login: String, password: String)
     }
+
+    interface View {
+        fun showConversationList()
+
+    }
 }
 
-class LoginController(val api: Login.Api) {
+class LoginController(val api: Login.Api, val view: Login.View) {
     fun onLogin(login: String, password: String) {
         api.login(login, password)
+        view.showConversationList()
     }
 }
