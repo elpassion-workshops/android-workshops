dde43532ab1904e73c4c114221c0ea3768ab740c
Marek Langiewicz <marek.langiewicz@gmail.com>
2016-09-11 12:13:30 +0200

    should not call api when login is empty

--- app/src/test/java/com/elpassion/secretmessenger/register/RegisterControllerTest.kt
+++ app/src/test/java/com/elpassion/secretmessenger/register/RegisterControllerTest.kt
@@ -1,8 +1,6 @@
 package com.elpassion.secretmessenger.register
 
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Test
 
 class RegisterControllerTest {
@@ -15,11 +13,22 @@ class RegisterControllerTest {
         verify(api, times(1)).register(login = "login")
     }
 
+    @Test
+    fun shouldNotCallApiWhenLoginIsEmpty() {
+        val api = mock<Register.Api>()
+        val controller = RegisterController(api)
+        controller.onRegister("")
+        verify(api, never()).register(any())
+    }
+
 }
 
 class RegisterController(val api: Register.Api) {
 
     fun onRegister(login: String) {
+        if (login.isEmpty()) {
+            return
+        }
        api.register(login)
     }
 }
