e9b6717a75b51f0ffadad81d35c4c1c579824378
Marek Langiewicz <marek.langiewicz@gmail.com>
2016-09-11 11:52:59 +0200

    Should call register on api

---
+++ app/src/test/java/com/elpassion/secretmessenger/register/RegisterControllerTest.kt
@@ -0,0 +1,30 @@
+package com.elpassion.secretmessenger.register
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.Test
+
+class RegisterControllerTest {
+
+    @Test
+    fun shouldCallRegisterOnApi() {
+        val api = mock<Register.Api>()
+        val controller = RegisterController(api)
+        controller.register()
+        verify(api, times(1)).register()
+    }
+}
+
+class RegisterController(val api: Register.Api) {
+    fun register() {
+       api.register()
+    }
+}
+
+interface Register {
+    interface Api {
+        fun register()
+    }
+
+}
